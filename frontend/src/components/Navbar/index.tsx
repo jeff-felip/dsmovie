bf80c84551520816d820b38f797dcdb354b8e04a
jefferson <jeff.keiryw@gmail.com>
2022-03-07 21:47:43 -0300

    Navbar

---
+++ frontend/src/components/Navbar/index.tsx
@@ -0,0 +1,21 @@
+import { ReactComponent as GitHubIcon } from "assecs/img/github.svg";
+import "./style.css"
+
+function Navbar() {
+    return (
+        <header>
+            <nav className="container">
+                <div className="dsmovie-nav-content">
+                    <h1>DSMovie</h1>
+                    <a href="https://github.com/jeff-felip/">
+                        <div className="dsmovie-contact-container">
+                            <GitHubIcon />
+                            <p className="dsmovie-contact-link">/JEFF-FELIP</p>
+                        </div>
+                    </a>
+                </div>
+            </nav>
+        </header>
+    );
+}
+export default Navbar;
